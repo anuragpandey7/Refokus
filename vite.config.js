1b1a96834323af081c6e58019f789e8306cdfb0d
Anurag Pandey <anurag001pandey@gmail.com>
2024-12-20 19:34:39 +0530

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,16 @@
 //   plugins: [react()],
 // })
 
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   build: {
+//     outDir: "dist",
+//   },
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -14,4 +24,9 @@ export default defineConfig({
   build: {
     outDir: "dist",
   },
+  server: {
+    port: 3000,
+  },
+  base: "/",
 });
+
